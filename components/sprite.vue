eec96145cc2f6df4ec959e15c17a669596f6b0d3
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-13 17:14:32 -0600

    practice w/ pages

---
+++ components/sprite.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img src="../static/icon.png">
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/journey.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>journeypage</h1>
+</template>

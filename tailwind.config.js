9264ead1114d19564d85916e3a404d9ee3445be9
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-19 17:10:10 -0600

    adds config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    theme: {
+      // fontFamily: {
+      //   'sans': ['upheaval', 'Helvetica', 'Arial', 'sans-serif']
+      // },
+    },
+    variants: {},
+    plugins: [],
+}

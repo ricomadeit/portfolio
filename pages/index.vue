38a17e7fa1abf4557185c0681566885f7df18483
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-19 12:39:37 -0600

    base for landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div>
+      <h1 class="text-white"> The Adventures of Rico </h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+}
 </script>
+
+<style>
+  html { 
+    background: url(../static/spaceBackground.gif) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+</style>

0fbb024a9e67625947f5f6542d74ac919fff4d52
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-26 15:54:31 -0600

    adds css dynamic styles

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <article class="prose upheaval">
+    <div :class="[`prose px-8 py-4 overflow-auto max-w-full h-screen ${slugBackground}`]">
+        <article>
             <nuxt-content :document="article" />
         </article>
-        <NuxtLink to="/"> Back Home </NuxtLink>
+        <NuxtLink to="/"> Back to space </NuxtLink>
     </div>
 </template>
 
@@ -13,6 +13,25 @@ export default {
     async asyncData({ $content, params }) {
         const article = await $content('ResumeContent', params.slug).fetch()
         return { article }
-    }
+    },
+    computed: {
+        slugBackground() {
+            switch (this.article.slug) {
+                case 'about':
+                    return 'bg-green-400'
+                case 'education':
+                    return 'bg-red-400'
+                case 'experience':
+                    return 'bg-gray-400'
+                case 'projects':
+                    return 'bg-indigo-300'
+                case 'skills':
+                    return 'bg-purple-400'
+                case 'summary':
+                    return 'bg-blue-400'
+                }
+            return 'bg-white'
+        }
+    },
 }
 </script>

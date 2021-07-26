57e919699b9c2466ad9ccedbe4335a92a5f8dbea
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-26 15:54:07 -0600

    changes translate elements

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,22 @@
       <h1 class="row-start-2 col-start-3 col-end-5 text-center text-5xl text-indigo-200">RICO'S ADVETURES</h1>
       <h3 class="row-start-4 col-start-3 col-end-5 text-center text-2xl text-indigo-500">Choose a planet</h3>
       <a class="row-start-1 col-start-1" href="/about">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/swamp.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/swamp.gif" />
       </a>
       <a class="row-start-2 col-start-6" href="/education">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/fire.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/fire.gif" />
       </a>
       <a class="row-start-1 col-start-4" href="/experience">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/moon.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/moon.gif" />
       </a>
       <a class="row-start-3 col-start-2" href="/skills">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/sea.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/sea.gif" />
       </a>
       <a class="row-start-4 col-start-5" href="/projects">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/ice.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/ice.gif" />
       </a>
       <a class="row-start-5 col-start-3" href="/summary">
-        <img class="transition duration-300 transform hover:translate-y-2" src="../static/planets/earth.gif" />
+        <img class="transition duration-300 transform hover:-translate-y-2" src="../static/planets/earth.gif" />
       </a>
     </div>
   </div>

a5f55ba288204cfdf49c48cdb1d5eb1b8ab56e12
Erik Halabi <eriknabilhalabi@gmail.com>
2021-07-27 17:19:58 -0600

    Initial dist subtree commit

---
+++ dist/_nuxt/static/1627427875/website/when/ready/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627427875",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"1d830369"}}}}("\u002Fwebsite\u002Fwhen\u002Fready"));

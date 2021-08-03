130eeb473c37df6d31ee3561d98964a93949ea4c
Erik Halabi <eriknabilhalabi@gmail.com>
2021-08-03 17:03:33 -0600

    dist folder

---
+++ dist/_nuxt/static/1628031760/experience/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F%3Crepository-name%3E\u002F_nuxt\u002Fstatic\u002F1628031760",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperience",config:{_app:{basePath:"\u002F%3Crepository-name%3E\u002F",assetsPath:"\u002F%3Crepository-name%3E\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"810d04e1"}}}}(null));
---
+++ dist/_nuxt/static/1628031760/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Feducation","\u002Fabout","\u002Fskills","\u002Fexperience","\u002Fsynopsis","\u002Fprojects"]})

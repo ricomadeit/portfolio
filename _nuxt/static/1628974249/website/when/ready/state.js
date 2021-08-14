04bba4bc53bfcbb4d50f9830b8303b88ef6770ba
Erik Halabi <eriknabilhalabi@gmail.com>
2021-08-14 14:50:55 -0600

    1d931d0

---
+++ _nuxt/static/1628974249/website/when/ready/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628974249",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"10518d8f"}}}}("\u002Fwebsite\u002Fwhen\u002Fready"));
